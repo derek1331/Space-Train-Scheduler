fbe89a40d4e1a17d595f709617f6cbd6a27a9e93
Derek Reed <1331derek@gmail.com>
2018-09-20 17:43:18 -0600

    prettify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,42 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBvqrwcBVao72fUUadOOWlcaudNXRGWgoA",
-    authDomain: "train-scheduler-956ec.firebaseapp.com",
-    databaseURL: "https://train-scheduler-956ec.firebaseio.com",
-    projectId: "train-scheduler-956ec",
-    storageBucket: "train-scheduler-956ec.appspot.com",
-    messagingSenderId: "258529099503"
-  };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBvqrwcBVao72fUUadOOWlcaudNXRGWgoA",
+  authDomain: "train-scheduler-956ec.firebaseapp.com",
+  databaseURL: "https://train-scheduler-956ec.firebaseio.com",
+  projectId: "train-scheduler-956ec",
+  storageBucket: "train-scheduler-956ec.appspot.com",
+  messagingSenderId: "258529099503"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 // Create a variable to reference the database.
-var database = firebase.database()
+var database = firebase.database();
 
 // 2. Button for adding Employees
 $("#add").on("click", function(event) {
   event.preventDefault();
 
   // Grabs user input
-  var trainName = $("#train-name").val().trim();
-  var trainDestination = $("#destination").val().trim();
-  var trainFirst = $("#first-train").val().trim();
-  var trainFrequency = $("#frequency").val().trim();
+  var trainName = $("#train-name")
+    .val()
+    .trim();
+  var trainDestination = $("#destination")
+    .val()
+    .trim();
+  var trainFirst = $("#first-train")
+    .val()
+    .trim();
+  var trainFrequency = $("#frequency")
+    .val()
+    .trim();
 
-  if (trainName === "" || trainDestination === "" || trainFirst === "" || trainFrequency === "" ) {
+  if (
+    trainName === "" ||
+    trainDestination === "" ||
+    trainFirst === "" ||
+    trainFrequency === ""
+  ) {
     return;
   }
 
@@ -35,10 +48,10 @@ $("#add").on("click", function(event) {
     frequency: trainFrequency
   };
 
-    // Uploads employee data to the database
-    database.ref().push(newTrain);
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
 
-    // Clears all of the text-boxes
+  // Clears all of the text-boxes
   $("#train-name").val("");
   $("#destination").val("");
   $("#first-train").val("");
@@ -70,15 +83,15 @@ database.ref().on("child_added", function(childSnapshot) {
   var nextTrainMin = trainFrequency - remainder;
 
   // format next arrival time
-  var nextTrainArrival = moment().add(nextTrainMin, "m").format("hh:mm A");
-
+  var nextTrainArrival = moment()
+    .add(nextTrainMin, "m")
+    .format("hh:mm A");
 
   // Train Info
   console.log(trainName);
   console.log(trainDestination);
   console.log(trainFirst);
   console.log(trainFrequency);
-
 
   // Create the new row
   var newRow = $("<tr>").append(
@@ -92,40 +105,4 @@ database.ref().on("child_added", function(childSnapshot) {
   // Append the new row to the table
   $("#table").append(newRow);
 });
-
-// $("input[type='text'], input[type='number'],input[type='date']").on("keyup", function(){
-//   if($("input[type='text']").val() != "" && $("input[type='number'").val() != "" && $("input[type='date'").val() != ""){
-//       $("input[type='submit']").removeAttr("disabled");
-//   } else {
-//       $("input[type='submit']").attr("disabled", "disabled");
-//   }
-// });
-
-// $("input[type='number']").on("change", function(){
-//   if($("input[type='text']").val() != "" && $("input[type='number']").val() != "" && $("input[type='date']").val() != ""){
-//       $("input[type='submit']").removeAttr("disabled");
-//   } else {
-//       $("input[type='submit']").attr("disabled", "disabled");
-//   }
-// });
-
-// $(document).ready(function() {
-//   var $form = $('form'), // cache
-//       $inputs = $form.find(':input'), // MOD extra caching
-//       $buttons = $inputs.not(':submit, :reset, :button'),
-//       $fields = $inputs.not($buttons),
-//       $submits = $buttons.filter(':submit');
-
-//   $submits.prop('disabled', true); // disable submit btn
-//   $fields.keyup(function() { // monitor all inputs for changes, MOD changed to keyup
-//        var disable = false;
-//        $fields.each(function(i, el) { // test all inputs for values
-//           if ($(el).val() === '') { // MOD use jQuery to get value
-//                   disable = true; // disable submit if any of them are still blank
-//                   return false; // MOD stop "each" if even one field is blank... less waste
-//           }
-//        });
-//        $submits.prop('disabled',disable);
-//       });
-// });
 
